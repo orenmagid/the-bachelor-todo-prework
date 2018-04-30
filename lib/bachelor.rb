3d376a08e42c1c6892d441856519de87af61935b
github username <github email address>
2018-04-30 15:19:02 +0200

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    contestant_hash.each do |key, value|
+      if key == "status"
+        if value == "Winner"
+          return contestant_hash["name"].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, array|
+    array.each do |contestant_hash|
+      contestant_hash.each do |key, value|
+        if key == "occupation"
+          if value == occupation
+            return contestant_hash["name"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  ht_array = []
+  data.each do |seasons, array|
+    array.each do |contestant_hash|
+      contestant_hash.each do |key, value|
+        if key == "hometown"
+          if value == hometown
+            ht_array << hometown
+          end
+        end
+      end
+    end
+  end
+  return ht_array.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons, array|
+    array.each do |contestant_hash|
+      contestant_hash.each do |key, value|
+        if key == "hometown"
+          if value == hometown
+            return contestant_hash["occupation"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  sum = 0
+
+    data.each do |seasons, array|
+      if seasons == season
+        array.each do |contestant_hash|
+          contestant_hash.each do |key, value|
+            if key == "age"
+              age_array << contestant_hash["age"].to_f
+
+            end
+          end
+        end
+      end
+    end
+    age_array.each { |a| sum = sum + a }
+  average = sum / age_array.length
+  average.round
+
 end
